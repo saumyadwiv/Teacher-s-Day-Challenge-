a5a820418194b6c10293ad3d528c1aa828e58fb2
saumyadwiv <132488903+saumyadwiv@users.noreply.github.com>
2025-07-26 20:01:23 +0530

    Create DAY1.cpp

---
+++ Beginner's Track/DAY1.cpp
@@ -0,0 +1,18 @@
+//**********************************Find Numbers with Even Number of Digits**********************
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+
+        int count=0;
+        for(int i=0;i<nums.size(); i++){
+            string s=to_string(nums[i]);
+            if(s.size()%2==0){
+                count++;
+            }
+        }
+        return count;
+
+
+        
+    }
+};
