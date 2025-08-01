4333df41e5fdd53628206f32c308f6daea611f6f
saumyadwiv <132488903+saumyadwiv@users.noreply.github.com>
2025-08-01 22:52:11 +0530

    Create WayTooLongWords.cpp

---
+++ Advanced Track/Day 7/WayTooLongWords.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+string process(string s){
+    string ans="";
+    if(s.length()<=10){
+        return s;
+    }
+    else{
+        ans+=s[0];
+        int charsInbetween= s.length()-2;
+        ans+=to_string(charsInbetween);
+        ans+=s[s.length()-1];
+
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<string> v;
+    
+    while(n--){
+
+        string s;
+        cin>>s;
+        v.push_back(s);
+
+        
+    }
+
+    for(int i=0;i<v.size();i++){
+        cout<<process(v[i])<<endl;
+    }
+
+    return 0;
+}
