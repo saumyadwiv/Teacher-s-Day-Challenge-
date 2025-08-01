fbbc928599ed0db5d589567d561bbed4a6ca41a6
saumyadwiv <132488903+saumyadwiv@users.noreply.github.com>
2025-08-01 22:59:13 +0530

    Create beautofulMatrix.cpp

---
+++ Advanced Track/Day 8/beautofulMatrix.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int arr[5][5];
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int ans=0;
+
+    for(int i=0 ;i<5; i++){
+        for(int j=0; j<5; j++){
+            if(arr[i][j]==1){
+                ans= abs(i-2) +abs(j-2);
+            }
+        }
+    }
+
+    cout<<ans;
+    return 0;
+}   
