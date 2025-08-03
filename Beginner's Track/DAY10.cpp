f44cc2f385556575afe8cfe239efbecbb6972b65
saumyadwiv <132488903+saumyadwiv@users.noreply.github.com>
2025-08-03 13:27:24 +0530

    Create DAY10.cpp

---
+++ Beginner's Track/DAY10.cpp
@@ -0,0 +1,29 @@
+///////////////// NEXT ROUND ////////////////////////
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,k;
+    cin>> n>>k;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int count=0;
+
+    int score= arr[k-1]; // score of  kth place finisher
+
+    for(int i=0; i<n; i++){
+        if(arr[i]>=score && arr[i]>0){
+            count++;
+        }
+    }
+
+    cout<<count<<endl;
+
+
+    return 0;
+}
