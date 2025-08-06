53328ae698e58537d39f0a0e4224c7adfbda0f32
saumyadwiv <132488903+saumyadwiv@users.noreply.github.com>
2025-08-06 10:37:39 +0530

    Create DAY12.cpp

---
+++ Beginner's Track/DAY12.cpp
@@ -0,0 +1,32 @@
+////////////////////PETYA AND STRINGS/////////////
+#include<iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string s1,s2;
+
+    cin>>s1;
+    cin>>s2;
+
+    transform(s1.begin(),s1.end(),s1.begin(),::tolower);
+    transform(s2.begin(),s2.end(),s2.begin(),::tolower);
+
+    if(s1<s2){
+        cout<<-1<<endl;
+    }
+
+    else if(s2<s1){
+
+        cout<< 1<<endl;
+
+    }
+
+    else{
+        cout<<0<<endl;
+    }
+
+
+    return 0;
+}
